add3c685d94d082794a20efe9761a250fe1ba6b1
Thomas Benjamin <105122168+Thomas-Benjamin@users.noreply.github.com>
2022-05-07 20:21:39 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Programa-1/main.c
@@ -0,0 +1,8 @@
+/* Esse é o meu primeiro programa! E isso é um bloco de comentário. Eles são usados para comentar sobre o código sem interferir em seu funcionamento.
+*/
+#include <stdio.h> // Aqui nós incluimos a biblioteca
+int main() //Aqui incluimos uma função
+{
+  printf("Hello \nWorld"); // Esse comando serve para escrever algo na tela
+  return 0; // Esse é o valor de retorno da função 
+}
---
+++ .ccls-cache/@home@runner@WoefulQuickwittedSolidstatedrive/main.c
@@ -0,0 +1,8 @@
+/* Esse é o meu primeiro programa! Esse bloco é um comentário. Eles podem ser usados para comentar sobre o código sem interferir em seu funcionamento.
+*/
+#include <stdio.h> // Aqui nós incluimos a biblioteca
+int main() //Aqui incluimos uma função
+{
+  printf("Hello \nWorld"); // Esse comando serve para escrever algo na tela
+  return 0; // Esse é o valor de retorno da função 
+}
